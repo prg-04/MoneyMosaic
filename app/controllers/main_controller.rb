b467a30963e1ac46056ec44d9ed451d9b4ed4ef3
prg-04 <karanjaevans95@gmail.com>
2023-12-09 06:35:06 +0300

    Add linter files

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,13 @@
 class MainController < ApplicationController
-
   before_action :verify_user
 
-  def index
-  end
+  def index; end
 
   private
 
   def verify_user
-    unless user_signed_in?
-      redirect_to splash_path
-    end
+    return if user_signed_in?
+
+    redirect_to splash_path
   end
 end
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,14 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class MainControllerTest < ActionDispatch::IntegrationTest
-  test "should get index if user is signed in" do
-  sign_in users(:one) # Assuming you have a fixture for users
-  get :index
-  assert_response :success
-end
+  test 'should get index if user is signed in' do
+    sign_in users(:one) # Assuming you have a fixture for users
+    get :index
+    assert_response :success
+  end
 
-test "should redirect to splash_path if user is not signed in" do
-  get :index
-  assert_redirected_to splash_path
-end
+  test 'should redirect to splash_path if user is not signed in' do
+    get :index
+    assert_redirected_to splash_path
+  end
 end
